b1118092cf3f6968334fcc257ad398107fff7e31
misraved <ved@turbot.com>
2023-12-12 22:34:47 +0530

    Update connection config variable definition to use the latest hcl syntax

--- openapi/connection_config.go
+++ openapi/connection_config.go
@@ -2,18 +2,10 @@ package openapi
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type openAPIConfig struct {
-	Paths []string `cty:"paths" steampipe:"watch"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"paths": {
-		Type: schema.TypeList,
-		Elem: &schema.Attribute{Type: schema.TypeString},
-	},
+	Paths []string `hcl:"paths" steampipe:"watch"`
 }
 
 func ConfigInstance() interface{} {
